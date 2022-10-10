9a050e3800c867485faaff64d7ed40a0afffcdab
Joseph Burga <joseph.burga.burga@student.ehb.be>
2022-10-10 16:59:17 +0200

    voleidig naam klaar exercises 1-1name

--- exercises/1-functions/1-1name.js
+++ exercises/1-functions/1-1name.js
@@ -16,7 +16,7 @@ function name() {
     context.lineTo(250, 50);
     context.moveTo(150, 50)
     context.lineTo(150, 250);
-    context.lineTo(50, 250)
+    context.lineTo(50, 250);
     context.stroke();
 
     //Letter O
@@ -26,13 +26,34 @@ function name() {
     context.moveTo(300, 50);
     context.lineTo(300, 250);
     context.lineTo(450, 250);
-    context.lineTo(450, 50)
-    context.lineTo(300, 50)
+    context.lineTo(450, 50);
+    context.lineTo(300, 50);
     context.stroke();
 
     //letter S
     context.lineWidth = 6;
     context.strokeStyle = "#208A5C";
     context.beginPath();
-    context.moveTo()
+    context.moveTo(700, 50);
+    context.lineTo(550, 50);
+    context.lineTo(550, 150);
+    context.moveTo(700, 150);
+    context.lineTo(550, 150);
+    context.moveTo(700, 150);
+    context.lineTo(700, 250);
+    context.lineTo(550, 250);
+    context.stroke();
+    
+    context.lineWidth = 6;
+    context.strokeStyle = "#7dae51";
+    context.beginPath();
+    context.moveTo(800, 50);
+    context.lineTo(800, 250);
+    context.moveTo(900, 50);
+    context.lineTo(800, 50);
+    context.moveTo(900, 150);
+    context.lineTo(800, 150);
+    context.moveTo(900, 250);
+    context.lineTo(800, 250);
+    context.stroke();
 }
